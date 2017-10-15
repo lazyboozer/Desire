ca721173aa34ce35217529efcca9971ab08a12f8
lazyboozer <dzin_dzin@mail.ru>
2017-10-15 20:25:49 +0700

    Edit commit date

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -45,8 +45,8 @@ const std::string CLIENT_NAME("Desire Core");
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID ""
-#define GIT_COMMIT_DATE "Tue, 12 10 2017 09:18"
+#define GIT_COMMIT_ID "41f1d09"
+#define GIT_COMMIT_DATE "Sun, 15 10 2017 1:18pm (UTC)"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
