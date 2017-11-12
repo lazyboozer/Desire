317a7c673635aaf054343c580ea76309c9dd72d6
lazyboozer <coin.developers@mail.ru>
2017-11-12 21:07:40 +0700

    Fix RPC port

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 9998;
+        nRPCPort = 9918;
     }
 };
 static CBaseMainParams mainParams;
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 19998;
+        nRPCPort = 19918;
         strDataDir = "testnet3";
     }
 };
