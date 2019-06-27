3830aa7d733530e63189803259da95c5e9014725
lazyboozer <32260138+lazyboozer@users.noreply.github.com>
2019-06-27 10:01:35 +0700

    Add GIT_COMMIT_ID

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -45,7 +45,7 @@ const std::string CLIENT_NAME("Desire Core");
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID ""
+#define GIT_COMMIT_ID "0b28618"
 #define GIT_COMMIT_DATE "Thu, 27 Jun 2019 09:00:00"
 #endif
 
